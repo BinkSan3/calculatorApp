bf6b3f1b08fb244175f24939dccd7d3b796f2ef0
J Meyer <124314330+BinkSan3@users.noreply.github.com>
2023-09-29 17:09:02 +0100

    Project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import { evaluate } from "mathjs";
 
-function App() {
+const App = () => {
+  const buttons = [
+    "1",
+    "2",
+    "3",
+    "-",
+
+    "4",
+    "5",
+    "6",
+    "+",
+
+    "7",
+    "8",
+    "9",
+    "/",
+
+    "clear",
+    "0",
+    "=",
+    "*",
+  ];
+  const [displayedNumber, setDisplayedNumber] = useState("");
+  const [result, setResult] = useState("");
+
+  const handleButtonClick = (press) => {
+    setDisplayedNumber(displayedNumber + press);
+
+    if (press == "=") {
+      const calculationResult = evaluate(displayedNumber);
+      setResult(calculationResult);
+      setDisplayedNumber(calculationResult.toString());
+    } else if (press == "clear") {
+      setDisplayedNumber("");
+      setResult("");
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2 id="calcBox">{displayedNumber}</h2>
+      <div id="buttonLayout">
+        {buttons.map((input) => (
+          <button key={input} onClick={() => handleButtonClick(input)}>
+            {input}
+          </button>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
